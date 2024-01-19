7f56b9dc63a25a7beb69cd7e4e6603beb44f4d5f
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-01-19 09:46:31 +0800

    adapt: adapt GMLIB 0.5.0

--- src/SuperFireworks.cpp
+++ src/SuperFireworks.cpp
@@ -1,24 +1,26 @@
 #include "Global.h"
 #include <GMLIB/Server/CompoundTagAPI.h>
 #include <GMLIB/Mod/CustomRecipe.h>
+#include <GMLIB/Server/ItemStackAPI.h>
 
 void RegisterFireworksRecipe() {
-    auto item = ItemStack{"minecraft:firework_rocket"};
-    item.setCustomLore({"", "§r§7由TNT制成的焯级大烟花", "§r§7动力强劲更持久！"});
-    item.setCustomName("§r§e焯级大烟花");
-    auto nbt = GMLIB_CompoundTag::getNbt(&item);
+    auto it = ItemStack{"minecraft:firework_rocket"};
+    auto item = (GMLIB_ItemStack*)&it;
+    item->setCustomLore({"", "§r§7由TNT制成的焯级大烟花", "§r§7动力强劲更持久！"});
+    item->setCustomName("§r§e焯级大烟花");
+    auto nbt = item->getNbt();
     nbt->getCompound("tag")->putCompound("Fireworks", CompoundTag{});
     nbt->getCompound("tag")->getCompound("Fireworks")->put("Explosions", ListTag{});
-    GMLIB_CompoundTag::setNbt(&item, nbt.get());
+    item->setNbt(nbt.get());
     std::vector<Recipes::Type> ingredients = {
         Recipes::Type("minecraft:paper", 'A', 1, 0)
     };
     for (int i = 0; i <= 7; i++) {
         ingredients.push_back(Recipes::Type("minecraft:tnt", 'B', 1, 0));
         std::string recipeId = "groupmountain:superfirework" + std::to_string(i + 1);
-        auto nbt = GMLIB_CompoundTag::getNbt(&item);
+        auto nbt = item->getNbt();
         nbt->getCompound("tag")->getCompound("Fireworks")->putByte("Flight", 5 * (i + 1));
-        GMLIB_CompoundTag::setNbt(&item, nbt.get());
-        GMLIB_CustomRecipe::registerLockedShapelessCraftingTableRecipe(recipeId, ingredients, &item);
+        item->setNbt(nbt.get());
+        GMLIB_CustomRecipe::registerLockedShapelessCraftingTableRecipe(recipeId, ingredients, item);
     }
 }
